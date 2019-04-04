aaa1e105ff774a3eca2053c31ca7456e02e45ba7
Michael Valverde <24768835+Mvalverde00@users.noreply.github.com>
2019-04-03 21:14:43 -0400

    Update Vignere.py

--- Vignere.py
+++ Vignere.py
@@ -24,5 +24,14 @@ def encode(message, passphrase):
 def decode(message, passphrase):
     return base(restrict(message), restrict(passphrase), 'd')
 
-print(encode("Best", "ZzZ"))
-        
+def print_stats(message, key):
+    print("Original message: {} ".format(restrict(message)))
+    print("key: {}".format(restrict(key)))
+    print("Encoded message: {}".format(encode(restrict(message),restrict(key))))
+
+def print_stats_decode(message, key):
+    print("Encoded message: {} ".format(restrict(message)))
+    print("key: {}".format(restrict(key)))
+    print("Decoded message: {}".format(decode(restrict(message),restrict(key))))
+    
+print_stats("Math seminar is the best", "Valve")
